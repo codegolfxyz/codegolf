ef6c7613809278215c7a8d86716d5ac937edb0c6
Kevin Nardi <nardi@apple.com>
2022-10-11 02:12:07 -0700

    let's try this i guess

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,15 +1,17 @@
 import pg from "pg";
 
 const url = process.env.DATABASE_URL!;
-const localhost = /.*localhost.*/.test(url);
-const reviewApp = !!process.env.HEROKU_PR_NUMBER;
-const useSSL = !localhost && !reviewApp;
-const pool = useSSL
-    ? new pg.Pool({
-        connectionString: `${url}?sslmode=require`,
-        ssl: { rejectUnauthorized: false }
-    })
-    : new pg.Pool({ connectionString: url });
+// const localhost = /.*localhost.*/.test(url);
+// const reviewApp = !!process.env.HEROKU_PR_NUMBER;
+// const useSSL = !localhost && !reviewApp;
+// const pool = useSSL
+//     ? new pg.Pool({
+//         connectionString: `${url}?sslmode=require`,
+//         ssl: { rejectUnauthorized: false }
+//     })
+//     : new pg.Pool({ connectionString: url });
+
+const pool = new pg.Pool({ connectionString: url });
 
 await pool.connect();
 const res = await pool.query('SELECT $1::text as message', ['Database connection successful!']);

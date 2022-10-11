0225c15b9289fe2c36881cc55fbc5bcce54db6d5
Kevin Nardi <nardi@apple.com>
2022-10-11 01:28:54 -0700

    Better SSL logic

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,10 +1,16 @@
 import pg from "pg";
 
-const connectionString = `${process.env.DATABASE_URL!}?sslmode=require`;
-const pool = new pg.Pool({
-    connectionString,
-    ssl: { rejectUnauthorized: !process.env.HEROKU_PR_NUMBER }
-});
+const url = process.env.DATABASE_URL!;
+const localhost = /.*localhost.*/.test(url);
+const reviewApp = !!process.env.HEROKU_PR_NUMBER;
+const useSSL = !localhost && !reviewApp;
+const pool = useSSL
+    ? new pg.Pool({
+        connectionString: `${url}?sslmode=require`,
+        ssl: { rejectUnauthorized: !reviewApp }
+    })
+    : new pg.Pool({ connectionString: url });
+
 await pool.connect();
 const res = await pool.query('SELECT $1::text as message', ['Database connection successful!']);
 console.log(res.rows[0].message);

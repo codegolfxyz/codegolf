713894833f84459f2e3c5530433c224eb7316f19
Kevin Nardi <nardi@apple.com>
2022-10-11 01:02:43 -0700

    Use SSL with connection string

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,6 +1,7 @@
 import pg from "pg";
 
-const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL! });
+const connectionString = `${process.env.DATABASE_URL!}?sslmode=require`;
+const pool = new pg.Pool({ connectionString });
 await pool.connect();
 const res = await pool.query('SELECT $1::text as message', ['Database connection successful!']);
 console.log(res.rows[0].message);

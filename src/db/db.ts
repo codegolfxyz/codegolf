5dc60af244b4c1cf3c9937683ccdeeb417763162
Kevin Nardi <nardi@apple.com>
2022-10-11 00:45:22 -0700

    PGSSLMODE=no-verify???

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,8 +1,5 @@
 import pg from "pg";
 
-console.log("HEYOOOOOO");
-console.log(process.env.DATABASE_URL);
-
 const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL! });
 await pool.connect();
 const res = await pool.query('SELECT $1::text as message', ['Database connection successful!']);
